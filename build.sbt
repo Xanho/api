23b8455b1957672a64c0d36dd5a8f1ec6e8aec37
Sean Cheatham <seancheatham@gmail.com>
2015-12-06 18:45:48 -0500

    SBT Dependencies for Slick and Evolutions

--- build.sbt
+++ build.sbt
@@ -1,20 +1,15 @@
 import sbt.Keys._
-import sbt.Project.projectToRef
-
-version := "1.0"
 
 lazy val root =
   (project in file("."))
     .enablePlugins(PlayScala)
     .settings(
       name := Settings.name,
-      scalaVersion := Settings.versions.scala,
+      version := Settings.versions.APPLICATION,
+      scalaVersion := Settings.versions.SCALA,
       scalacOptions ++= Settings.scalacOptions,
       libraryDependencies ++= Settings.dependencies.library,
       resolvers ++= Settings.resolvers,
-      organization := Settings.organization
+      organization := Settings.organization,
+      routesGenerator := InjectedRoutesGenerator
     )
-
-// Play provides two styles of routers, one expects its actions to be injected, the
-// other, legacy style, accesses its actions statically.
-routesGenerator := InjectedRoutesGenerator
--- project/Settings.scala
+++ project/Settings.scala
@@ -1,14 +1,20 @@
 import play.sbt.PlayImport._
 import sbt._
 
+/**
+  * Container for SBT or organizational configuration settings
+  */
 object Settings {
 
+  /**
+    * Application Name
+    */
   val name =
     "api"
 
-  val version =
-    "0.0.1"
-
+  /**
+    * Scala compiler options
+    */
   val scalacOptions =
     Seq(
       "-Xlint",
@@ -17,19 +23,53 @@ object Settings {
       "-feature"
     )
 
+  /**
+    * Package prefix/organization
+    */
   val organization =
     "org.xanho"
 
+  /**
+    * Custom resolvers
+    */
   val resolvers =
     Seq("scalaz-bintray" at "http://dl.bintray.com/scalaz/releases")
 
+  /**
+    * Versions
+    */
   object versions {
 
-    val scala =
+    val APPLICATION =
+      "0.0.1"
+
+    val SCALA =
       "2.11.7"
+
+    /**
+      * Library dependency versions
+      */
+    object dependencies {
+
+      val PLAY_SLICK =
+        "1.1.1"
+
+      val JWT_PLAY =
+        "0.4.1"
+
+      val MYSQL_CONNECTOR_JAVA =
+        "5.1.36"
+
+      val SCALA_BCRYPT =
+        "2.4"
+
+    }
 
   }
 
+  /**
+    * Project Dependencies
+    */
   object dependencies {
 
     val library =
@@ -37,8 +77,14 @@ object Settings {
         jdbc,
         cache,
         ws,
-        specs2 % Test
+        specs2 % Test,
+        "com.github.t3hnar" % "scala-bcrypt_2.10" % versions.dependencies.SCALA_BCRYPT,
+        "mysql" % "mysql-connector-java" % versions.dependencies.MYSQL_CONNECTOR_JAVA,
+        "com.pauldijou" %% "jwt-play" % versions.dependencies.JWT_PLAY,
+        "com.typesafe.play" %% "play-slick" % versions.dependencies.PLAY_SLICK,
+        "com.typesafe.play" %% "play-slick-evolutions" % versions.dependencies.PLAY_SLICK
       )
 
   }
+
 }
